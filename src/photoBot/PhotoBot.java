9642008a9eeef9ade872cf2a51c3e6ad4e767cf8
11crom11 <d_danigarcia@hotmail.es>
2018-06-20 20:12:59 +0200

    - Manda fotos y reenvia fotos

--- src/photoBot/PhotoBot.java
+++ src/photoBot/PhotoBot.java
@@ -1,6 +1,14 @@
 package photoBot;
 
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import org.telegram.telegrambots.api.methods.GetFile;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.api.objects.File;
+import org.telegram.telegrambots.api.objects.PhotoSize;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
@@ -31,6 +39,35 @@ public class PhotoBot extends TelegramLongPollingBot {
 	            e.printStackTrace();
 	        }
 	    }
+		else if (update.getMessage().hasPhoto()){//Si hemos recibido una foto...
+			List<PhotoSize> lFotos = update.getMessage().getPhoto();
+			PhotoSize ps = lFotos.get(0);
+	        GetFile getFileMethod = new GetFile();
+	        getFileMethod.setFileId(ps.getFileId());
+	        
+	        try {
+	            // We execute the method using AbsSender::execute method.
+	            File file = execute(getFileMethod);
+	            //https://github.com/rubenlagus/TelegramBots/wiki/FAQ
+	            //https://www.mkyong.com/java/how-to-write-an-image-to-file-imageio/
+	            ImageIO.(bufferedImage, "jpg", new File());
+	            // We now have the file_path
+	            //return file.getFilePath();
+	        } catch (TelegramApiException e) {
+	            e.printStackTrace();
+	        }
+	        
+			SendPhoto message = new SendPhoto()
+								.setChatId(update.getMessage().getChatId())
+								.setPhoto(lFotos.get(0).getFileId());
+			
+			try {
+				sendPhoto(message);
+			} catch (TelegramApiException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 	}
 

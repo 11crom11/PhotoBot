b45f2285158d8634633b55464669a98c653a8bd8
11crom11 <d_danigarcia@hotmail.es>
2018-06-21 19:49:42 +0200

    - Ahora se guardan las imagenes recibidas en una galeria de imagenes del servidor, una carpeta por cada id de usuario y el archivo se llama FECHA.jpeg

--- src/photoBot/PhotoBot.java
+++ src/photoBot/PhotoBot.java
@@ -1,5 +1,8 @@
 package photoBot;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.imageio.ImageIO;
@@ -41,18 +44,37 @@ public class PhotoBot extends TelegramLongPollingBot {
 	    }
 		else if (update.getMessage().hasPhoto()){//Si hemos recibido una foto...
 			List<PhotoSize> lFotos = update.getMessage().getPhoto();
-			PhotoSize ps = lFotos.get(0);
+			
+			PhotoSize ps = lFotos.stream()
+						.sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+						.findFirst()
+						.orElse(null);
+			
 	        GetFile getFileMethod = new GetFile();
 	        getFileMethod.setFileId(ps.getFileId());
-	        
+	  
 	        try {
 	            // We execute the method using AbsSender::execute method.
 	            File file = execute(getFileMethod);
+	            
+	            java.io.File fileJava = downloadFile(file.getFilePath());           
+	            //BufferedImage bImg = new BufferedImage(830, 467, BufferedImage.TYPE_INT_ARGB);
+	            
 	            //https://github.com/rubenlagus/TelegramBots/wiki/FAQ
 	            //https://www.mkyong.com/java/how-to-write-an-image-to-file-imageio/
-	            ImageIO.(bufferedImage, "jpg", new File());
-	            // We now have the file_path
-	            //return file.getFilePath();
+	            
+	            try {
+					//bImg = ImageIO.read(fileJava);
+	            	java.io.File dir = new java.io.File("./galeria/" + update.getMessage().getFrom().getId());
+	            	dir.mkdirs();
+	            	
+	            	ImageIO.write(ImageIO.read(fileJava), "jpeg", new java.io.File(dir.getPath() + "/" + update.getMessage().getDate() + ".jpeg"));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+
 	        } catch (TelegramApiException e) {
 	            e.printStackTrace();
 	        }

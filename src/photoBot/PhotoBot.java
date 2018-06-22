5072cf24250e528ff875eb35e6f721ef32c26e88
Iván Aguilera Calle <ivan.aguilera103@gmail.com>
2018-06-22 18:59:14 +0200

    - finalizada y refactorizada funcion guardarImagen

--- src/photoBot/PhotoBot.java
+++ src/photoBot/PhotoBot.java
@@ -20,17 +20,13 @@ public class PhotoBot extends TelegramLongPollingBot {
 
 	@Override
 	public String getBotUsername() {
-		// TODO Auto-generated method stub
-		
 		String nombreBot = "PhotosDASI_bot";
-		
 		
 		return nombreBot;
 	}
 
 	@Override
 	public void onUpdateReceived(Update update) {
-		// TODO Auto-generated method stub
 		
 		if (update.hasMessage() && update.getMessage().hasText()) {
 	        SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
@@ -43,50 +39,26 @@ public class PhotoBot extends TelegramLongPollingBot {
 	        }
 	    }
 		else if (update.getMessage().hasPhoto()){//Si hemos recibido una foto...
+			boolean ok = false;
 			List<PhotoSize> lFotos = update.getMessage().getPhoto();
+			Long chatID = update.getMessage().getChatId();
+			Integer userID = update.getMessage().getFrom().getId();
+			Integer date = update.getMessage().getDate();
 			
-			PhotoSize ps = lFotos.stream()
-						.sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
-						.findFirst()
-						.orElse(null);
+			ok = this.guardarFotoEnServidor(lFotos, chatID, userID, date);
 			
-	        GetFile getFileMethod = new GetFile();
-	        getFileMethod.setFileId(ps.getFileId());
-	  
-	        try {
-	            // We execute the method using AbsSender::execute method.
-	            File file = execute(getFileMethod);
-	            
-	            java.io.File fileJava = downloadFile(file.getFilePath());           
-	            //BufferedImage bImg = new BufferedImage(830, 467, BufferedImage.TYPE_INT_ARGB);
-	            
-	            //https://github.com/rubenlagus/TelegramBots/wiki/FAQ
-	            //https://www.mkyong.com/java/how-to-write-an-image-to-file-imageio/
-	            
-	            try {
-					//bImg = ImageIO.read(fileJava);
-	            	java.io.File dir = new java.io.File("./galeria/" + update.getMessage().getFrom().getId());
-	            	dir.mkdirs();
-	            	
-	            	ImageIO.write(ImageIO.read(fileJava), "jpeg", new java.io.File(dir.getPath() + "/" + update.getMessage().getDate() + ".jpeg"));
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-
-	        } catch (TelegramApiException e) {
-	            e.printStackTrace();
-	        }
-	        
-			SendPhoto message = new SendPhoto()
-								.setChatId(update.getMessage().getChatId())
-								.setPhoto(lFotos.get(0).getFileId());
+			SendMessage message = new SendMessage().setChatId(chatID);
+			
+			if(ok){
+				message.setText("Ya he recibido y almacenado correctamente tus imágenes.");
+			}
+			else{
+				message.setText("Ha ocurrido un error :( ... ¿Puedes mandarme de nuevo las imágenes?");
+			}
 			
 			try {
-				sendPhoto(message);
+				execute(message);
 			} catch (TelegramApiException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -95,11 +67,42 @@ public class PhotoBot extends TelegramLongPollingBot {
 
 	@Override
 	public String getBotToken() {
-		// TODO Auto-generated method stub
-		
 		String token = "530885872:AAHq86bNy6Yx08zpPuFXfTfBdE9YtlN90cI";
 				
 		return token;
 	}
+	
+	
+	private boolean guardarFotoEnServidor(List<PhotoSize> lFotos, Long idChat, Integer idUsuario, Integer fecha){
+		boolean ok = true;
+		
+		PhotoSize ps = lFotos.stream()
+				.sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+				.findFirst()
+				.orElse(null);
 
+		GetFile getFileMethod = new GetFile();
+		getFileMethod.setFileId(ps.getFileId());
+
+		try {
+			File file = execute(getFileMethod);
+			java.io.File fileJava = downloadFile(file.getFilePath());           
+			//https://github.com/rubenlagus/TelegramBots/wiki/FAQ
+			//https://www.mkyong.com/java/how-to-write-an-image-to-file-imageio/
+			
+			//bImg = ImageIO.read(fileJava);
+			java.io.File dir = new java.io.File("./galeria/" + idUsuario);
+			dir.mkdirs();
+			ImageIO.write(ImageIO.read(fileJava), "jpeg", new java.io.File(dir.getPath() + "/" + fecha + ".jpeg"));
+			
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+			ok = false;
+		} catch (IOException e) {
+			e.printStackTrace();
+			ok = false;
+		}
+		
+		return ok;
+	}
 }

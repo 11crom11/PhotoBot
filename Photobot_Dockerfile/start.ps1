8f5a191a3e379f929c83291d579cf6c1609bbbf0
ivanClass <ivan.aguilera103@gmail.com>
2018-09-05 23:22:16 +0200

    -añadidos dockefile y archivos necesarios

---
+++ Photobot_Dockerfile/start.ps1
@@ -0,0 +1,3 @@
+cd C:\PhotoBot\
+git pull
+java  -Djava.library.path="C:\PhotoBot\opencv\build\lib\Release" -Dgate.plugins.home="C:\PhotoBot\GATE\plugins" -Dgate.site.config="C:\PhotoBot\GATE\gate.xml" -jar Photobot.jar

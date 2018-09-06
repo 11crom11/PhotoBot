44a68da9a36543e3b712864ad7dc70b797f2870f
Iván Aguilera Calle <ivan.aguilera103@gmail.com>
2018-09-06 07:31:56 +0200

    Add files via upload
    pruebas finales docker

--- Photobot_Dockerfile/start.ps1
+++ Photobot_Dockerfile/start.ps1
@@ -1,3 +1,4 @@
 cd C:\PhotoBot\
 git pull
-java  -Djava.library.path="C:\PhotoBot\opencv\build\lib\Release" -Dgate.plugins.home="C:\PhotoBot\GATE\plugins" -Dgate.site.config="C:\PhotoBot\GATE\gate.xml" -jar Photobot.jar
+Start-Process -NoNewWindow mongod --bind_ip_all
+java -cp '.;.\Photobot.jar' '-Djava.library.path=C:\PhotoBot\opencv\build\lib\Release' '-Dgate.plugins.home=C:\PhotoBot\GATE\plugins' '-Dgate.site.config=C:\PhotoBot\GATE\gate.xml' -jar Photobot.jar
